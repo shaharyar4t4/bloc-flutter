9a233255abf72890442203a2e7e154c705aefe6b
shaharyar <shaharyar4t4@gmail.com>
2026-02-26 12:55:36 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/learningbloc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.learningbloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Bloc',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
+}
+
