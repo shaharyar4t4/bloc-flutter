cc19714902ee5d5b12baeb330470353e94e39f85
shaharyar <shaharyar4t4@gmail.com>
2026-02-27 09:43:24 +0500

    complete the flow of Bloc function

--- lib/bloc/internet_bloc/internet_bloc.dart
+++ lib/bloc/internet_bloc/internet_bloc.dart
@@ -5,5 +5,13 @@ import 'package:learningbloc/bloc/internet_bloc/internet_state.dart';
 class InternetBloc extends Bloc<InternetEvent, InternetState> {
 
   // constructor
-  InternetBloc(): super( InternetInitialState()){}
+  InternetBloc(): super(InternetInitialState()){
+
+    //on is basically to check "Jab Internet lost hoga to ye ON chala ga"
+    on<InternetLostEvent>((event, emit) => null);
+    
+    // on is basically to check "Jab Internet gain hoga to ye ON chala ga"
+    on<InternetGainedEvent>((event, emit) => null);
+
+  }
 }
