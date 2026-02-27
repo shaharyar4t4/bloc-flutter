47c4c96ca092b27f06d8a9296b75607209e83f3c
shaharyar <shaharyar4t4@gmail.com>
2026-02-27 10:58:09 +0500

    update code

--- lib/bloc/internet_bloc/internet_bloc.dart
+++ lib/bloc/internet_bloc/internet_bloc.dart
@@ -1,17 +1,41 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:learningbloc/bloc/internet_bloc/internet_event.dart';
 import 'package:learningbloc/bloc/internet_bloc/internet_state.dart';
 
+// jab koi screen khatam hoti ha tu bloc automatically close hoti ha but ".listen" close nhi is ko handing karna kiya hum use karta ha streamsubscription ko..
 class InternetBloc extends Bloc<InternetEvent, InternetState> {
+  // this comes form the Packages
+  Connectivity _connectivity = Connectivity();
+  StreamSubscription? connectivitySubscription;
 
   // constructor
-  InternetBloc(): super(InternetInitialState()){
-
+  InternetBloc() : super(InternetInitialState()) {
     //on is basically to check "Jab Internet lost hoga to ye ON chala ga"
-    on<InternetLostEvent>((event, emit) => null);
-    
-    // on is basically to check "Jab Internet gain hoga to ye ON chala ga"
-    on<InternetGainedEvent>((event, emit) => null);
+    // emit is back to transfer the data to UI
+    on<InternetLostEvent>((event, emit) => emit(InternetLostState()));
 
+    // on is basically to check "Jab Internet gain hoga to ye ON chala ga"
+    // emit is back to transfer the data to UI
+    on<InternetGainedEvent>((event, emit) => emit(InternetGainedState()));
+
+   connectivitySubscription = _connectivity.onConnectivityChanged.listen((result) {
+      // if user agr connect ho mobile ye wifi se tu ye statement chali gi....
+      if (result == ConnectivityResult.mobile ||
+          result == ConnectivityResult.wifi) {
+        add(InternetGainedEvent());
+      } else {
+        add(InternetLostEvent());
+      }
+    });
+  }
+
+  // ye function is time work karti jab ap bloc(me screen) close hoti 
+  @override
+  Future<void> close() {
+    connectivitySubscription?.cancel();
+    return super.close();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learningbloc/bloc/internet_bloc/internet_bloc.dart';
 import 'package:learningbloc/screen/home_screen.dart';
 
 void main() {
@@ -10,13 +12,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Bloc',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return BlocProvider(
+      // the create line means Internet Bloc avilable raha ga me app me khehe bha chala jayo
+      create: (context) => InternetBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Bloc',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: HomeScreen(),
       ),
-      home: HomeScreen(),
     );
   }
 }
